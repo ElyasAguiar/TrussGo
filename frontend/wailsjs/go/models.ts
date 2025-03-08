ac57a57f7f989381d699b8e761a388652ec54d08
ElyasAguiar <elyas.santana98@academico.ifs.edu.br>
2025-03-08 01:46:22 -0300

    feat: Add member management APIs and update navigation styles

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -16,6 +16,40 @@ export namespace models {
 	        this.y = source["y"];
 	    }
 	}
+	export class Member {
+	    id: number;
+	    startNode: Node;
+	    endNode: Node;
+	
+	    static createFrom(source: any = {}) {
+	        return new Member(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.startNode = this.convertValues(source["startNode"], Node);
+	        this.endNode = this.convertValues(source["endNode"], Node);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 
 }
 
